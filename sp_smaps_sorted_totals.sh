eeaca0550d7f29f85d65a623db99b7d2aeca9303
Eero Tamminen <eero.tamminen@nokia.com>
2010-10-13 13:22:31 +0300

    allow regex to be used for SMAPS field line, major update to usage help

--- sp_smaps_sorted_totals.sh
+++ sp_smaps_sorted_totals.sh
@@ -4,12 +4,17 @@ usage ()
 {
 	name=${0##*/}
 	echo
-	echo "$name <mapped file|mapping name> <SMAPS field> <SMAPS snapshot file>"
+	echo "$name <SMAPS mapping pattern> <SMAPS field pattern> <SMAPS snapshot file>"
 	echo
-	echo "Shows size-sorted totals for given mapping for all the processes"
+	echo "Shows size-sorted totals for given mappings for all the processes"
 	echo "in the given SMAPS snapshot file."
 	echo
-	echo "SMAPS field should be one of:"
+	echo "SMAPS mapping pattern can have anything from the mapping line that comes"
+	echo "_after_ the given mapping's address range; access rights, file name etc."
+	echo "Characters that are special for regular expressions '[].+*', need to"
+	echo "be quoted with '\\' if they are supposed to be matched literally!"
+	echo
+	echo "SMAPS field pattern needs to start with one of the SMAPS field names:"
 	echo "  Size, Rss, Pss, Shared_Clean, Shared_Dirty,"
 	echo "  Private_Clean, Private_Dirty, Referenced, Swap."
 	echo
@@ -20,6 +25,10 @@ usage ()
 	echo "  $name '.*' Swap smaps.cap"
 	echo "- what processes have largest heaps:"
 	echo "  $name '\[heap\]' Size smaps.cap"
+	echo "- which processes' executable code sections are writable:"
+	echo "  $name ' rwxp ' Size smaps.cap"
+	echo "- total of given sized anonymous allocs (unnamed mappings) in processes:"
+	echo "  $name ' 0 \$' 'Size: *2044 ' smaps.cap"
 	echo
 	echo "ERROR: $1!"
 	echo
@@ -29,20 +38,22 @@ usage ()
 if [ $# -ne 3 ]; then
 	usage "wrong number of arguments"
 fi
-if [ -z $(echo "$2"|fgrep -e Size -e Rss -e Pss -e Shared_Clean -e Shared_Dirty -e Private_Clean -e Private_Dirty -e Referenced -e Swap) ]; then
-	usage "unknown SMAPS mapping value type"
+if [ -z "$(echo ""$2""|grep -e '^Size' -e '^Rss' -e '^Pss' -e '^Shared_Clean' -e '^Shared_Dirty' -e '^Private_Clean' -e '^Private_Dirty' -e '^Referenced' -e '^Swap')" ]; then
+	usage "unknown SMAPS field used in '$2'"
 fi
 if [ \! -f "$3" ]; then
-	usage "file '$2' doesn't exist"
+	usage "file '$3' doesn't exist"
 fi
 
-mapping=$(echo "$1"|sed 's%/%\\/%g')
-field="$2"
+mapping=$(echo "$1"|sed 's%/%\\/%g')	# quote awk pattern delimiters
+field="${2%%:*}"			# SMAPS field used for checking
+line="$2"				# full SMAPS field line to match
 file="$3"
 
 heading="Size:\t\tPID:\tName:\n"
 
-echo "finding process totals for type '$field' in '$mapping' mappings..."
+echo "finding process totals for field '$field' matching line '$line'"
+echo "in '$mapping' mappings from file '$file'..."
 echo
 printf $heading
 
@@ -56,30 +67,32 @@ function mapping_usage () {
 /^#Name/ {
 	mapping_usage();
 	name = $2;
+	next;
 }
 /^#Pid/ {
 	mapping_usage();
 	pid = $2;
+	next;
 }
 # hex address range, stuff, mapping name
-/^[0-9a-f]+-[0-9a-f].*'"$mapping"'/ {
+/^[0-9a-f]+-[0-9a-f]+.*'"$mapping"'/ {
 	mapping = 1;
 	map = $6;
+	next;
 }
-/^'"$field"':/ {
+/^'"$line"'/ {
 	if (mapping > 0) {
 		mapping = 0;
 		size += $2;
 	}
+	next;
+}
+/^'"$field"'/ {
+	mapping = 0;
+	next;
 }
 END {
 	mapping_usage();
 }
 ' "$file"|sort -n
 printf $heading
-
-if [ \! -z "$(echo \"$mapping\"|fgrep -e '[' -e ']' -e '*' -e '+' -e '.')" ]; then
-	echo
-	echo "WARNING: mapping name '$mapping' contained regular expression special characters."
-	echo "If results look unexpected, make sure these characters were escaped properly!"
-fi
